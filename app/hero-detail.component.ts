ede9ebcf78b286681d3f6783bbbf6c9a7bbf1ff6
oneRice <onericeyifan@gmail.com>
2016-09-01 11:55:41 +0200

    Http - hero

--- app/hero-detail.component.ts
+++ app/hero-detail.component.ts
@@ -16,17 +16,22 @@ export class HeroDetailComponent implements OnInit {
 
   constructor(
     private route: ActivatedRoute,
-    private heroservice: HeroService) { }
+    private heroService: HeroService) { }
 
   ngOnInit():void {
     this.route.params.forEach((params: Params) => {
       let id = +params['id'];
-      this.heroservice.getHero(id)
+      this.heroService.getHero(id)
         .then(hero => this.hero = hero)
     });
   }
 
   goBack(): void {
     window.history.back();
+  }
+
+  save(): void {
+  this.heroService.update(this.hero)
+    .then(this.goBack);
   }
 }
--- app/heroes.component.ts
+++ app/heroes.component.ts
@@ -35,4 +35,24 @@ export class HeroesComponent implements OnInit {
     let link = ['/detail', this.selectedHero.id];
     this.route.navigate(link);
   }
+
+  add(name:string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.create(name)
+      .then(hero => {
+        this.heroes.push(hero);
+        this.selectedHero = null;
+      });
+  }
+
+  delete(hero:Hero): void {
+    this.heroService.delete(hero.id)
+      .then(() => {
+        this.heroes = this.heroes.filter(h => h !== hero);
+        if (this.selectedHero === hero) {
+          this.selectedHero = null;
+        }
+      });
+  }
 }
