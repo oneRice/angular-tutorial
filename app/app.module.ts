bd4d960424cef39f6b9236625695345c4032e82d
oneRice <onericeyifan@gmail.com>
2016-09-08 21:02:12 +0200

    use firebase as database

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AngularFireModule } from 'angularfire2';
 
 import './rxjs-extentions';
 
@@ -19,12 +20,20 @@ import { HeroService } from './hero.service';
 
 import { routing } from './app.route';
 
+export const firebaseConfig = {
+  apiKey: "AIzaSyBzUX1-p4G35uvUS-HnvP6AS6dxGGglW0U",
+  authDomain: "angular2-tutorial-37d81.firebaseapp.com",
+  databaseURL: "https://angular2-tutorial-37d81.firebaseio.com",
+  storageBucket: "angular2-tutorial-37d81.appspot.com",
+};
+
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
     InMemoryWebApiModule.forRoot(InMemoryDataService),
+    AngularFireModule.initializeApp(firebaseConfig),
     routing
   ],
   declarations: [
--- app/hero.service.ts
+++ app/hero.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Headers, Http } from '@angular/http';
+import { AngularFire, FirebaseObjectObservable } from 'angularfire2';
 
 import 'rxjs/add/operator/toPromise';
 
