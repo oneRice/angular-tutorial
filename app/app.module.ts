7760595d05b22be21272b92c7bb26c7e0ecf1957
oneRice <onericeyifan@gmail.com>
2016-09-17 12:36:04 +0200

    omit angular fire

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { AngularFireModule } from 'angularfire2';
+// import { AngularFireModule } from 'angularfire2';
 
 import './rxjs-extentions';
 
@@ -33,7 +33,7 @@ export const firebaseConfig = {
     FormsModule,
     HttpModule,
     InMemoryWebApiModule.forRoot(InMemoryDataService),
-    AngularFireModule.initializeApp(firebaseConfig),
+    // AngularFireModule.initializeApp(firebaseConfig),
     routing
   ],
   declarations: [
